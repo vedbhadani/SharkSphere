a9cd3f56717ea7dfdeb1effaef2fce2c324e7da8
vedbhadani <vedbhadani157@gmail.com>
2026-03-29 01:12:40 +0530

    fix: add PATCH to allowed CORS methods for admin approval

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
